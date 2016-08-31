b2865767f1a50e42095357d317626fb3ff713e23
rahulgayatri23 <rahulgayatri84@gmail.com>
2016-08-31 11:44:08 +0530

    Check if the board has been filled or not.

--- ticTackToe1.py
+++ ticTackToe1.py
@@ -4,12 +4,17 @@ import sys
 import os
 from datetime import date
 
+#**********************************************************************************************************************
 def print_hzline(boardSize) : 
         print(' --- '*boardSize) 
 
+
+#**********************************************************************************************************************
 def print_vrline(boardSize) : 
         print('|    '*(boardSize+1)) 
 
+
+#**********************************************************************************************************************
 def printBoardSkeleton(boardSize) : 
         for index in range(0,boardSize) : 
                 print_hzline(boardSize)
@@ -17,6 +22,8 @@ def printBoardSkeleton(boardSize) :
         print_hzline(boardSize)
         print('\n')
 
+
+#**********************************************************************************************************************
 def fillBoard(boardSize) : 
 
         firstRow = list(input('Enter the first row elements seperated by a comma : '))
@@ -26,6 +33,8 @@ def fillBoard(boardSize) :
 
         return board
 
+
+#**********************************************************************************************************************
 def InitStage(boardSize) : 
         firstRow = [0,0,0]
         secondRow = [0,0,0]
@@ -35,6 +44,8 @@ def InitStage(boardSize) :
 
         return board
 
+
+#**********************************************************************************************************************
 def printBoard(board,boardSize) : 
         print('The Tick Tac Toe board is : \n')
 
@@ -46,6 +57,7 @@ def printBoard(board,boardSize) :
                 print('\n')
 
 
+#**********************************************************************************************************************
 def diagonalMatch(board, boardSize) : 
 
         if board[0][0] != 0 : 
@@ -67,6 +79,7 @@ def diagonalMatch(board, boardSize) :
         return 0
 
 
+#**********************************************************************************************************************
 def rowMatch(board, boardSize) : 
 
         for i in range (0, boardSize) : 
@@ -78,6 +91,7 @@ def rowMatch(board, boardSize) :
         return 0
 
 
+#**********************************************************************************************************************
 def colMatch(board, boardSize) : 
 
         for i in range(0,boardSize) : 
@@ -91,6 +105,8 @@ def colMatch(board, boardSize) :
 
         return 0
 
+
+#**********************************************************************************************************************
 #Matches if either a row or a column or a diagonal has been filled by a particular player. 
 def whoIsWinner(board, boardSize) : 
         
@@ -114,6 +130,7 @@ def whoIsWinner(board, boardSize) :
         return winner
 
 
+#**********************************************************************************************************************
 #Checks if the the input given by the user is within the bounds.
 def isInBound(playerInput, boardSize) : 
         if int(playerInput[0]) >= boardSize or int(playerInput[2]) >= boardSize : 
@@ -121,10 +138,13 @@ def isInBound(playerInput, boardSize) :
         else : 
                 return 1
 
+
+#**********************************************************************************************************************
 #Adds input from the user to the board. Checks if it is in the bounds of the board and if the input is being given at the right place or not. 
 def addInput(board, playerInput, boardSize, index) : 
-        while  isInBound(playerInput, boardSize) and board[int(playerInput[0])][int(playerInput[2])] != 0 : 
+        while  (isInBound(playerInput, boardSize)) and (board[int(playerInput[0])][int(playerInput[2])] != 0 ) : 
                 playerInput = list(raw_input('The position given by you is already filled. Try again: '))
+
 
         if(isInBound(playerInput, boardSize)) : 
                 if index%2 == 0 : 
@@ -134,6 +154,8 @@ def addInput(board, playerInput, boardSize, index) :
         else : 
                 print('Player %d : Loose your chance as you are out of bounds'%(index%2+1))
 
+
+#**********************************************************************************************************************
 #Main funcition that plays the game
 def playGame(board, boardSize) : 
         index = 0
@@ -141,28 +163,40 @@ def playGame(board, boardSize) :
         gameSts = str(raw_input('\'C\' for Continue, \'Q\' for Quit : '))
 
         while(gameSts != 'Q') : 
-                if index%2 == 0 : 
-                        playerInput = list(raw_input('Player 1 enter your input : '))
-                        addInput(board, playerInput, boardSize, index)
-                        printBoard(board, boardSize)
-                        winner = whoIsWinner(board, boardSize)
-                        if winner != 0 : 
-                                return winner
-                else : 
-                        playerInput = list(raw_input('Player 2 enter your input : '))
-                        addInput(board, playerInput, boardSize, index)
-                        printBoard(board, boardSize)
-                        winner = whoIsWinner(board, boardSize)
-                        if winner != 0 : 
-                                return winner
+                index = 0
+                while (index < boardSize*boardSize) : 
+                        if index%2 == 0 : 
+                                playerInput = list(raw_input('Player 1 enter your input : '))
+                                addInput(board, playerInput, boardSize, index)
+                                printBoard(board, boardSize)
+                                winner = whoIsWinner(board, boardSize)
+                                if winner != 0 : 
+                                        return winner
+                        else : 
+                                playerInput = list(raw_input('Player 2 enter your input : '))
+                                addInput(board, playerInput, boardSize, index)
+                                printBoard(board, boardSize)
+                                winner = whoIsWinner(board, boardSize)
+                                if winner != 0 : 
+                                        return winner
 
-                index+=1
-                gameSts = str(raw_input('\'C\' for Continue, \'Q\' for Quit : '))
+                        index+=1
+
+                        if index >= boardSize*boardSize : 
+                                print('\t All the positions are filled. If you want to play start again')
+                                gameSts = str(raw_input('\'C\' for Continue, \'Q\' for Quit : '))
+                                if gameSts == 'C' : 
+                                        board = InitStage(boardSize)
+                                        printBoard(board, boardSize)
+                        else: 
+                                gameSts = str(raw_input('\'C\' for Continue, \'Q\' for Quit : '))
+
 
         print('\n Quiting the game')
         return winner
 
 
+#**********************************************************************************************************************
 def main() :
                 print('********* X - represents player 1 \n********* Y - represents player 2')
                 print('********* Alternatively player 1 and 2 will enter their input into the board')
@@ -180,5 +214,8 @@ def main() :
                 #Main winner for the game
                 winner = playGame(board, boardSize)
 
+
+
+#**********************************************************************************************************************
 if __name__ == '__main__' : 
         main()
